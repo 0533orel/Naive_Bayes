8ad532122db60bf7d55e3644ea7cebdc20ed8fd5
orel <0533orel@gmail.com>
2025-07-10 10:29:26 +0300

    create class ModelTester and all around it

--- csv_data_loader.py
+++ csv_data_loader.py
@@ -1,18 +1,29 @@
 import pandas as pd
 
 class CsvDataLoader:
-    def __init__(self, path: str):
+    def __init__(self, path: str, target_col: str):
         try:
             self.df = pd.read_csv(path)
+            self.target_col = target_col
         except Exception as e:
-            self.df = None
             raise ValueError(f"Problem loading file: {e}")
+
+        self.clean_data()
 
     def clean_data(self):
         if self.df is None:
             raise ValueError("The DataFrame is empty.")
 
-        mask = self.df.columns.str.contains('id', case=False)
+        mask = self.df.columns.str.contains('index|id', case=False)
         self.df = self.df.loc[:, ~mask]
 
         self.df = self.df.dropna()
+
+        self.df = self.df.loc[:, ~self.df.T.duplicated()]
+
+        if self.target_col not in self.df.columns:
+            raise ValueError(f"'{self.target_col}' column not found in DataFrame.")
+
+        if self.df.columns[-1] != self.target_col:
+            cols = [c for c in self.df.columns if c != self.target_col] + [self.target_col]
+            self.df = self.df[cols]
